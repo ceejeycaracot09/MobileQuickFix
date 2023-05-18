5411137edff048d66321b12730d77fd482e27f60
ceejeycaracot09 <116365096+ceejeycaracot09@users.noreply.github.com>
2023-05-18 08:47:33 +0800

    Initial Commit for Machine Number 2

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Stack, Button } from '@react-native-material/core';
+import { TextInput } from "@react-native-material/core";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+
+const App = () => (
+  
+  <Stack fill center spacing={4}>
+    
+    <Text variant="h1" style={{ margin: 16 }}>
+    Registration
+    </Text>
+    <TextInput label="Username" style={{ margin: 16 }} />
+    <TextInput label="Password" style={{ margin: 16 }} />
+    <Button title="Sign Up" onPress={() => alert("Sign up Successfully")}/>
+    <Text variant="h3" style={{ margin: 16 }}>
+    Don't have an account yet?
+    </Text>
+  </Stack>
+)
+export default App;
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Quick Fix</Text>
+      
+
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+//}
 
 const styles = StyleSheet.create({
   container: {
